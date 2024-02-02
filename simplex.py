740c7c8ebf2ece43a31e8841f367cbe4a2265681
Matheus <matheushamada@live.com>
2024-02-01 22:26:33 -0300

    Pegando as matrizes B, b e a função resposta

---
+++ simplex.py
@@ -0,0 +1,34 @@
+import numpy as np
+
+Function = []
+B = []
+b = []
+
+def Step1():
+    print()
+
+print("Insira os itens da função, separados por espaços:")
+function = input()
+values = function.split()
+Function.extend(int(value) for value in values)
+
+print("Agora, coloque a matriz das variáveis:")
+while True:
+    line = input()
+    if line == "":
+        break
+
+    values = line.split()
+    B.append([int(value) for value in values])
+
+print("Por fim, a matriz b dos resultados:")
+results = input()
+values = results.split()
+b.extend(int(value) for value in values)
+print(b)
+b = np.array(b).T
+
+print(B)
+print(np.linalg.inv(B))
+
+print(Function)
